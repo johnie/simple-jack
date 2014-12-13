2cdf4b03dd49f8c6472193b571837656e8fd14f2
Johnie Hjelm <johnie.hjelm@symbio.com>
2014-12-13 16:10:42 +0100

    Simple Jack has many messages

--- simple-jack.js
+++ simple-jack.js
@@ -12,8 +12,18 @@ Jack.Simple = (function () {
       // Create the audio element
       var audio = document.createElement('AUDIO');
 
+      // Simple Jack messages
+      var messages = [
+        "https://dl.dropboxusercontent.com/s/nc7zhg5uolvvg9j/simplejack-svenska.m4a",
+        "https://dl.dropboxusercontent.com/s/yaims8r1gecxhe0/simplejack-happy.m4a",
+        "https://dl.dropboxusercontent.com/s/1bk6cbhsiqgcq6i/simplejack-tears.m4a"
+      ];
+
+      // Choose message
+      var randMessage = messages[Math.floor(Math.random() * messages.length)];
+
       // Audio properties
-      audio.src = "https://dl.dropboxusercontent.com/s/nc7zhg5uolvvg9j/simplejack-svenska.m4a";
+      audio.src = randMessage;
       audio.autoplay = true;
 
       // Container
