fbc65cb9259976736762976d83e6f0dbc2764823
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-07 13:52:58 +0100

    Added more messages

--- simple-jack.js
+++ simple-jack.js
@@ -5,18 +5,24 @@
 var Jack = Jack || {};
 
 Jack.Simple = (function () {
+  'use strict';
 
   // Initiate
   var Message =  {
+    
     init: function () {
+      
       // Create the audio element
       var audio = document.createElement('AUDIO');
 
       // Simple Jack messages
       var messages = [
-        "http://dl.dropboxusercontent.com/s/nc7zhg5uolvvg9j/simplejack-svenska.m4a",
-        "http://dl.dropboxusercontent.com/s/yaims8r1gecxhe0/simplejack-happy.m4a",
-        "http://dl.dropboxusercontent.com/s/1bk6cbhsiqgcq6i/simplejack-tears.m4a"
+        'http://dl.dropboxusercontent.com/s/nc7zhg5uolvvg9j/simplejack-svenska.m4a',
+        'http://dl.dropboxusercontent.com/s/yaims8r1gecxhe0/simplejack-happy.m4a',
+        'http://dl.dropboxusercontent.com/s/1bk6cbhsiqgcq6i/simplejack-tears.m4a',
+        'https://dl.dropboxusercontent.com/s/20rakco0axa9ztn/simplejack-retardalert.m4a',
+        'https://dl.dropboxusercontent.com/s/6u6ws2z0zswpi80/simplejack-cripplefight.m4a',
+        'http://dl.dropboxusercontent.com/s/2jid88eyr2oiaf3/simplejack-timmy.m4a'
       ];
 
       // Choose message
@@ -28,14 +34,18 @@ Jack.Simple = (function () {
 
       // Container
       var container = document.body;
-
       container.appendChild(audio);
+
     },
     load: function () {
+
       setTimeout(this.init, Math.random() * 5000 + 2000);      
+
     }
+
   };
 
   // Load the messages
   Message.load();
+  
 })();
